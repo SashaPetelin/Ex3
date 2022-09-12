b770e0f7216a7c2a3fe2e7965d572b55b6f98e0e
Sasha Petelin <alexanderzr58@icloud.com>
2022-09-12 19:51:49 +0300

    Example#23

---
+++ Homework3_Ex3_C#.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i=1;
+
+while (i<=number)
+{
+    int cube = (int)Math.Pow(i, 3);
+    i++;
+    Console.Write(cube + " ");
+}
